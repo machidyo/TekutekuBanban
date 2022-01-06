a3f1da9b4d56259d1b19b6885130f9ab4220e270
Yorihiko Machida <yorihiko.machida@curiosity-inc.jp>
2022-01-07 02:37:02 +0900

    add: Test SendDataToPeers

--- Assets/Scripts/Game/NetworkManager.cs
+++ Assets/Scripts/Game/NetworkManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Niantic.ARDK.AR;
 using Niantic.ARDK.AR.ARSessionEventArgs;
@@ -8,6 +9,7 @@ using Niantic.ARDK.AR.Networking.ARNetworkingEventArgs;
 using Niantic.ARDK.Networking;
 using Niantic.ARDK.Networking.MultipeerNetworkingEventArgs;
 using Niantic.ARDK.Utilities;
+using Niantic.ARDK.Utilities.BinarySerialization;
 using TMPro;
 using UnityEngine;
 
@@ -41,6 +43,8 @@ public class NetworkManager : MonoBehaviour
 
         arNetworking.PeerStateReceived += OnPeerStateReceived;
         arNetworking.PeerPoseReceived += OnPeerPoseReceived;
+
+        multipeerNetworking.PeerDataReceived += OnPeerDataReceived;
         
         sessionIdInputField.text = $"{Random.Range(100, 1000)}";
     }
@@ -57,6 +61,23 @@ public class NetworkManager : MonoBehaviour
         var sessionId = sessionIdInputField.text;
         var sessionIdAsByte = Encoding.UTF8.GetBytes(sessionId);
         multipeerNetworking.Join(sessionIdAsByte);
+    }
+
+    public void OnSendDataToPeersButtonClicked()
+    {
+        using var stream = new MemoryStream();
+        GlobalSerializer.Serialize(stream, "mojimojikun");
+        multipeerNetworking.SendDataToPeers(0, stream.ToArray(), multipeerNetworking.OtherPeers, TransportType.UnreliableOrdered);
+    }
+
+    private void OnPeerDataReceived(PeerDataReceivedArgs args)
+    {
+        if (args.Tag == 0)
+        {
+            using var stream = new MemoryStream(args.CopyData());
+            var str = (string)GlobalSerializer.Deserialize(stream);
+            Debug.Log(str);
+        }
     }
 
     private void OnSessionRan(ARSessionRanArgs args)
