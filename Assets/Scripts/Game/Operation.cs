bcc93a031c98ae5c68e044409915f99a9abbc911
Yorihiko Machida <yorihiko.machida@curiosity-inc.jp>
2022-01-21 01:00:07 +0900

    refactor: デバッグログを削除

--- Assets/Scripts/Game/Operation.cs
+++ Assets/Scripts/Game/Operation.cs
@@ -37,18 +37,15 @@ public class Operation : MonoBehaviour
 
     private void SetMarker()
     {
-        Debug.Log($"START SetMarker (true, false) {networkManager.IsHost}, {networkManager.CanStart}");
         if (!networkManager.IsHost) return;
         if (networkManager.CanStart) return;
         
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out var hit, 100))
         {
-            Debug.Log($"hit.collider.gameObject.layer = {hit.collider.gameObject.layer}");
             if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Floor"))
             {
                 var pos = hit.point + Vector3.up * 0.1f;
-                Debug.Log($"pos = {pos}, Marker is null = {Marker == null}");
                 if (Marker == null)
                 {
                     Marker = Instantiate(markerGameObject, pos, Quaternion.identity);
